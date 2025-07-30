881f0cad22fa8b3485265ec9c605ac1f97bc585f
Mihirpathakji <mihirgithub123456@gmail.com>
2025-07-30 07:44:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pattern/pattern.cpp
+++ Difficulty: Medium/Pattern/pattern.cpp
@@ -5,7 +5,7 @@ class Solution {
     for(int i =1;i<=n;i++){
            
         // for the spaces :
-        // (3-1)==2 is correct there are two spaces in the left most of the first row 
+        
         for(int j =1;j<=(n-i);j++){
             printf(" ");        
         }
@@ -23,20 +23,15 @@ class Solution {
     
     }
     
-
-    // i==3-1==2  i.e   i == 2     >=1   for the two times the loop will run that's what we wanted actually 
-
         for(int i=n;i>=1;i--){
             // print the spaces 
             
             for(int j=1;j<=(n-i);j++){ 
-                 // j<=(3-2)<=1  only one time the loop will run here 
                 printf(" ");
             }
             
             //print the stars :
-            for(int j=1;j<=i;j++){
-                //j<==(3) for the 3 times it runs and thereby prints the star 
+            for(int j=1;j<=i;j++){ 
                 printf("* ");
             }
             
