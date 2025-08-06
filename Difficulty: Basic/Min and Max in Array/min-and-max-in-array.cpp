32d1a753788923b676c3c3d64f83a26044bae461
Mihirpathakji <mihirgithub123456@gmail.com>
2025-08-06 13:40:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.cpp
@@ -6,18 +6,23 @@ class Solution {
     int mini=INT_MAX;
     for(int i =0;i<arr.size();i++){
         if(arr[i]<mini){
-            mini =arr[i];
+            mini =arr[i];  // mini == arr[i] which is lesser than the INT_MAX
+            
+            // i.e the mini = 1 we store the 1 in that variable mini  and now for the
+            //next iteration it would be check that arr[1]<1(mini) if yes than we will 
+            //update the arr[i] with the mini and hence we updated that arr[i] within that
+            // mini
+            
+            
         }
     }
-    
-    
-    
-    
-    
     int maxi=INT_MIN;
     for(int i =0;i<arr.size();i++){
         if(arr[i]>maxi){
-            maxi=arr[i];
+            maxi=arr[i]; // maxi =1 then it would be checked in the net iteration that 
+            //arr[1] >maxi if yes means that arr[i] is the posssible to be the maximum of
+            // all these array elements. so there we store the or update the maxi with that 
+            //value of the arr[i]
         }
     }
     
