5845a3bd711cc3c5a5b1609fb7c4fbde7a43f99a
Mihirpathakji <mihirgithub123456@gmail.com>
2025-08-21 11:52:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.cpp
+++ Difficulty: Easy/Prime Number/prime-number.cpp
@@ -8,6 +8,9 @@ class Solution {
                 count++;
                 if(i!=n/i){
                     count++;
+                }// if count has become greater than 2 than no need to check that now it is increasing or not no need to check for the other iterations already the count had exceeed the 2 so it is not a prime number moved to next input exit that for loop forever for that input 
+                if(count>2){
+                    break;
                 }
             }
         }
