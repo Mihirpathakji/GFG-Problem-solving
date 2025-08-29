e83c05b7ea33875b4e8f1a2f92eca7cb17300d04
Mihirpathakji <mihirgithub123456@gmail.com>
2025-08-29 17:43:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -2,19 +2,13 @@
 class Solution {
   public:
     int nthFibonacci(int n) {
-        // 0 1 1 2 3 5 8 13 21 
-        // For the fib(0) and fib(1)  ->you need to cover it in an edge case as we 
-        // cannot handle them in the formula the formula will give the answer from the
-        // 2nd index itslef 
-        if(n==0 || n==1)return n;
-        int a=0;
-        int b=1;
-        int sum;
-        for(int i =2;i<=n;i++){
-            sum = a+b;
-            a=b;
-            b=sum;
-        }    
-        return sum;
+        if(n<=1)
+        {
+            return n;
+        }
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    
+    // Tc:O(2^n)
+       
     }
-};                                                                                      
+};
