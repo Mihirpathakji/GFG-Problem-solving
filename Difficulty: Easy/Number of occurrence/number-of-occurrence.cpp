bebf0e1080a76e692469a71b133eab08b03837db
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-25 00:45:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,62 +1,17 @@
 class Solution {
   public:
     int countFreq(vector<int>& arr, int target) {
+        int lb=lower_bound(arr.begin(),arr.end(),target)-arr.begin();
+        int up=upper_bound(arr.begin(),arr.end(),target)-arr.begin();;
+        //First_Occurance=lb;Last_Occurance=up-1;// 0 1 ->length=(1-0)+1
         
-// We are given an sorted array and target value      
-        // Whenever there is  a sorted array and a target is given to us then it is obvious to apply the binary search 
-        int start = 0;
-        int end = arr.size() -1;
-        int mid ;
-    
-        int ans1 =-1 ; 
-        
-        // jo kasu bi nahi ave toh 0 toh rehsejjjjjj
-        int ans2 = -1;
-        
-        // Right most index
-        
-        while(start<=end){
-                mid = start + (end-start)/2;
-                if(arr[mid] == target){
-                    ans1 = mid; // store that index in ans than look for the further right most
-                    start = mid + 1;
-                }        
-                else if(arr[mid] > target){
-                        end = mid -1;
-                }   
-                else{
-                    start = mid + 1 ;
-                }
+        if(lb==up)
+        {
+            return 0;//No occurance of target 
         }
-        
-// Now the values of the start end all gets changed bcz of the above while loop so reinitialize them for the next while loop
-    
-      start  = 0;
-      end = arr.size() -1;
-      while(start <= end){
-             mid  = start + (end - start)/2;  // you find out the middle index check if the value at that middle index do equal the target value or not 
-            if(arr[mid] == target){
-    // now if i write ans  = mid then ans got initialized by the new value of the mid but the previously storedf value of the ans = previous mid got lost so how can i return that better you made two different variable for the last and the first index 
-    // For the two different mid the answers  it is better to store them into two different conatiners since you need to subtract them if both of them are ubder a single container imagine how would you subtract them tghereby adding one to them 
-    // So the better way is to stored them under two different containers thereby subtract them individually 
-                ans2 = mid;
-                end = mid -1;        
-            }
-                else if(arr[mid] > target){
-                        end = mid -1;
-                }   
-                else{
-                    start = mid + 1 ;
-                }
-            
-      }
-        // AKhi while loop pati gayi toh pn na thayu i.e there is no target in the whole array :
-        
-        if(ans1 == -1 ||ans2 ==-1){
-                return 0;
-        }
-        
-        return (ans1-ans2)+1;
-        
+        int ans=(up-1)-lb+1;
+        return ans;
+        //TC:O(log2(n))
+        //SC:O(1)
     }
 };
