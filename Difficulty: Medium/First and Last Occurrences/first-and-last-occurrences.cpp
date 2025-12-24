a9ce1c08ca3485b0be20d3eca9b66e8bbe865024
Mihirpathakji <mihirgithub123456@gmail.com>
2025-12-24 19:29:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
@@ -1,55 +1,15 @@
 class Solution {
   public:
     vector<int> find(vector<int>& arr, int x) {
-        int s = 0;
-        int e = arr.size() -1;
-        int leftoccurence = -1;
-        int rightoccurence = -1;
-        
-        int mid ;
-        
-        // For the right :
-        while(s <= e){
-            mid  = s + (e-s)/2;
-        
-            if(arr[mid] == x){
-                rightoccurence  = mid; // store that index 
-                s = mid  + 1;  // search on the right side
-            }
-            else if(arr[mid] > x){
-                    e  = mid - 1;
-            }
-            else {
-                s = mid  + 1 ;
-            }
-            
+        int lb=lower_bound(arr.begin(),arr.end(),x)-arr.begin();
+        int up=upper_bound(arr.begin(),arr.end(),x)-arr.begin();
+        if(lb==up)
+        {
+            //Target value is not present in the Array in this case.
+            return {-1,-1};
         }
-        
-        // Reset e and s since their values of e and s gets changed after  the use of the first while loop
-        
-        // e and s both gets changed after the use of the first while loop :
-         e = arr.size() -1;
-         s = 0;
-         
-         
-                // For the left:
-        while(s <= e){
-            mid  = s + (e-s)/2;
-        
-            if(arr[mid] == x){
-                leftoccurence = mid; // store that index 
-                e = mid -1;  // search on the left side
-            }
-            else if(arr[mid] > x){
-                    e  = mid - 1;
-            }
-            else {
-                s = mid  + 1 ;
-            }
-            
-        }
-        
-        return {leftoccurence,rightoccurence};
-        
+        return {lb,up-1};
+        //TC:O(log2(n))
+        //SC:O(1)
     }
 };
