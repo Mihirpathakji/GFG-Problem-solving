c5809300d7e6173162d071b8191590be45fea639
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-02 00:29:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -11,59 +11,37 @@ public:
         right = nullptr;
     }
 };
-
-
 */
+
 
 class Solution {
   public:
+ 
+   void left_view(Node *root,int level,vector<int>& ans)
+   {
+        if(root == NULL)
+        {
+            return;
+        }
+        
+        if(level == ans.size())
+        {
+            ans.push_back(root->data);
+        }
+        
+        left_view(root->left,level+1,ans);//Left_subtree
+        left_view(root->right,level+1,ans);//Right_subtree
+       
+   }
   
     vector<int> leftView(Node *root) {
         
-        //answer : first node of every level.
-        //Level order Traversal:
-        
+        //Do with DFS : we need to track the level everytime at every recursive call.
+        int level = 0;
         vector<int>ans;
-        if(root == NULL)
-        {
-            return ans;
-        }
-        
-        queue<Node*>q;
-        q.push(root);
-        
-        while(!q.empty())
-        {
-            //Traverse every level and picks up it's first element and insert it into ans.
-            
-            int n = q.size();
-            int original = n;//1 5 4
-           
-            while(n--)
-            {
-                Node* temp = q.front();
-                q.pop();
-                
-                if(n == original-1)
-                {
-                    ans.push_back(temp->data);
-                }
-                
-                if(temp->left)
-                {
-                    q.push(temp->left);
-                }
-                
-                if(temp->right)
-                {
-                    q.push(temp->right);
-                }
-                
-            }
-            
-        }
+        left_view(root,level,ans);
         
         return ans;
-        
     }
+    
 };
