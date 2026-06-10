b5af24479b0b23e91ddab351ad54a91f31e16728
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-10 20:26:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check K-th Bit/check-kth-bit.cpp
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.cpp
@@ -1,30 +1,15 @@
 class Solution {
   public:
     bool checkKthBit(int n, int k) {
-      
-      //Brute force :
-    
-      vector<int>binary;
-      //Dividing successively by base in which i wanted to convert.
-      while(n > 0)
-      {
-        int remainder = n%2;//001;1
-        binary.push_back(remainder);//001;1
-        n/=2;//1;0
-      }
-      
-      //0010000000000000000
-      //0000000000000100
-      
-      for(int i = 0;i < binary.size() ; i++)
-      {
-          if(i == k && binary[i] == 1)
-          {
-                return true; 
-          } 
-      }
-      
-      return false;
+        
+        //Let's check if the kth bit from the rightside is set or not using Right Shift.
+        
+        //Using Right shift Operator.
+        
+        return ((n >> k) & 1);
+        
+        //TC : O(1)
+        //SC : O(1)
         
     }
 };
