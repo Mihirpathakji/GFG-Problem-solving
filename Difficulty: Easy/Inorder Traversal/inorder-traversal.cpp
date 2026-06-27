68a4cac59884878c413d75cbe2500123ec58994d
Mihirpathakji <mihirgithub123456@gmail.com>
2026-06-27 20:18:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Inorder Traversal/inorder-traversal.cpp
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.cpp
@@ -15,30 +15,46 @@ class Node {
 
 class Solution {
   public:
-    
-    Node* recursive_inorder(Node* root,vector<int>&ans)
-    {
-        if(!root)
-        {
-            return NULL;
-        }
-        
-        //left,root,right.
-        
-        recursive_inorder(root->left,ans);
-        ans.push_back(root->data);
-        recursive_inorder(root->right,ans);
-            
-        return root;    
-
-    }
-  
     vector<int> inOrder(Node* root) {
         
-        
         vector<int>ans;
-        recursive_inorder(root,ans);
-        return ans;
         
+        while(root)
+        {
+            //Case1 :
+            
+            if(!(root->left))
+            {
+                ans.push_back(root->data);
+                root = root->right;
+            }
+            else
+            {
+                //Check if the Left subtree is traversed or not.
+                    
+                Node* temp = root->left;
+                while(temp->right && temp->right!=root)
+                {
+                    temp = temp->right;
+                }
+                
+                if(temp->right == NULL)
+                {
+                    temp->right = root;
+                    root = root->left;
+                }
+                else
+                {
+                    //loop is already there.Remove the linkage.
+                    //temp->right = root.
+                    temp->right = NULL;
+                    ans.push_back(root->data);
+                    root = root->right;
+                }
+            }
+        }
+        return ans;
+     
+     
     }
 };
