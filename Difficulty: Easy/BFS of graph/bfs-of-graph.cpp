d6053ab5bc02871250c706e6fcbabecb0e1876c5
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-03 00:31:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,32 +1,37 @@
 class Solution {
   public:
+    
     vector<int> bfs(vector<vector<int>> &adj) {
         
-        //1.Queue 2.vector which stores Queue visited elements. 3.vector<>
-      
-      vector<int>ans;//To keep track of the visited queue elements.
-      queue<int>q;
-      vector<bool>visited(adj.size(),false);
+        int n = adj.size();
+        vector<bool>visited(n,false);
         
-     q.push(0);       
-     visited[0] =  true;
-     
-     while(!q.empty())
-     {
-        ans.push_back(q.front());//In General.
-        q.pop();//[]
+        vector<int>ans;
         
-        //We will go to the adjacency list of the ans[last element]
-        for(auto it : adj[ans[ans.size()-1]])
-        {
-            if(!visited[it])
-            {
-                visited[it] = true;
-                q.push(it);
+        queue<int>q;
+        
+        q.push(0);
+        visited[0] = true;
+        
+        while(!q.empty()) {
+            
+            int temp = q.front();//0
+            q.pop();
+            
+            ans.push_back(temp);
+            
+            for(int &it : adj[temp]) {
+                
+                if(!visited[it]) {
+                    q.push(it);
+                    visited[it] = true;
+                }
             }
         }
-     }
+        
         return ans;
         
+        
+      
     }
 };
