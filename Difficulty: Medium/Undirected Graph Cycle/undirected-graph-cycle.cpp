58dda79c814074cdc25eee6b9ed889e071d8426f
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-11 20:20:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,25 +1,25 @@
 class Solution {
   public:
   
-    bool detect_cycle(int node,int parent_node,unordered_map<int,vector<int>>& adj_list,vector<bool>& visited) {
+    bool bfs_detectcycle(int start, int parent, unordered_map<int,vector<int>>& adj_list,vector<bool>& visited) {
         
-        visited[node] = true;
+        queue<pair<int,int>>q;//{start node,where it came from}.
+        q.push({start,parent});
+        visited[start] = true;
         
-        for(auto v : adj_list[node]) {
+        while(!q.empty()) {
             
-            //If v is parent of node.Than no need to detect cycle.
-            if(v == parent_node) {
-                continue;
-            }
+            int node = q.front().first;
+            int parent_node = q.front().second;
+            q.pop();
             
-            //If it is already visited.->Cycle detected.
-            else if(visited[v] == true) {
-                return true;
-            }
-            
-            else {
-                if(detect_cycle(v,node,adj_list,visited)) {
-                    return true;
+            for(auto adjacent_node : adj_list[node]) {
+                if( visited[adjacent_node] == false) {
+                    q.push({adjacent_node,node});
+                    visited[adjacent_node] = true;
+                }
+                else if(visited[adjacent_node] == true && adjacent_node != parent_node) {
+                    return true;                    
                 }
             }
         }
@@ -27,9 +27,11 @@ class Solution {
         return false;
         
     }
-    
+  
   
     bool isCycle(int V, vector<vector<int>>& edges) {
+        
+        vector<bool>visited(V,false);
         
         unordered_map<int,vector<int>>adj_list;
         
@@ -42,23 +44,22 @@ class Solution {
             adj_list[v].push_back(u);
         }
         
-        int parent_node = -1;
-        vector<bool>visited(V,false);
+        int parent = -1;//dummy parent for the start.
+        //There may be Presense of Unconnected components so we need to do the 
+        //BFS for every unvisited vertices.
         
         for(int i = 0;i < V;i++) {
-            
             if(!visited[i]) {
-                if(detect_cycle(i,-1,adj_list,visited)) {
+                
+                //Every unvisited vertex is liable for a bfs.
+                
+                if(bfs_detectcycle(i,parent,adj_list,visited)){
                     return true;
                 }
             }
-            
         }
         
         return false;
-        
-        //TC : O(V)
-        //SC : O(V)
-        
+
     }
 };
