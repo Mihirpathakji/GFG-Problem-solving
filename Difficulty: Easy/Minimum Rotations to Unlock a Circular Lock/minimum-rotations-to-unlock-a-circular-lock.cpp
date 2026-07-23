aba85239269d65b0b27e76e45897e9e6b5f6e4bf
Mihirpathakji <mihirgithub123456@gmail.com>
2026-07-23 11:12:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum Rotations to Unlock a Circular Lock/minimum-rotations-to-unlock-a-circular-lock.cpp
+++ Difficulty: Easy/Minimum Rotations to Unlock a Circular Lock/minimum-rotations-to-unlock-a-circular-lock.cpp
@@ -1,89 +1,27 @@
 class Solution {
   public:
-    
     int rotationCount(int r, int d) {
         
-        int min_rotation = 0;
+        int ans = 0;
+        int first = 0;
+        int second = 0;
         
-        vector<int>r_digits;
-        vector<int>d_digits;
-    
-        
-        while(r > 0) {
-            int rem1 = r % 10;
+        while(max(r,d)) {
+            
+            first = r%10;
+            second = d%10;
+            
             r/=10;
-            r_digits.push_back(rem1);
-        }
-        
-        reverse(r_digits.begin(),r_digits.end());
-        
-         
-        while(d > 0) {
-            int rem1 = d % 10;
             d/=10;
-            d_digits.push_back(rem1);
-        }
-        
-        reverse(d_digits.begin(),d_digits.end());
-        
-        int r_digits_size = r_digits.size();
-        int d_digits_size=  d_digits.size();
-        
-        vector<int>new_r_digits;
-        vector<int>new_d_digits;
-        
-        bool flag1 = false;
-        bool flag2 = false;
-        
-        if(r_digits_size > d_digits_size) {
             
-            flag1 = true;
-            int new_appended = r_digits_size-d_digits_size;
-            
-            for(int i = 0;i < new_appended;i++) {
-                new_d_digits.push_back(0);
-            }
-            
-            for(int i = 0;i < d_digits.size();i++) {
-                new_d_digits.push_back(d_digits[i]);
-            }
+            ans += min(abs(first-second),10-abs(first-second));
             
         }
         
-        else if(d_digits_size > r_digits_size) {
-            
-            flag2 = true;
-            int new_appended = d_digits_size - r_digits_size;
-            
-            for(int i = 0;i < new_appended;i++){
-                new_r_digits.push_back(0);
-            }
-            
-            for(int i = 0;i < r_digits.size();i++) {
-                new_r_digits.push_back(r_digits[i]);
-            }
-            
-        }
+        return ans;
+    
+        //TC : O(log(max(r,d)))
+        //SC : O(1)
         
-        if(!(flag1 || flag2)) {
-            new_r_digits = r_digits;
-            new_d_digits = d_digits;
-        }
-        else {
-            if(flag1 == true) {
-                new_r_digits = r_digits;
-            }
-            else {
-                new_d_digits = d_digits;
-            }
-        }
-        
-        for(int i = 0;i < new_r_digits.size();i++) {
-            min_rotation += min(abs(new_r_digits[i]-new_d_digits[i]),10-abs(new_r_digits[i]-new_d_digits[i]));
-        }
-     
-     
-        return  min_rotation;
-     
     }
 };
