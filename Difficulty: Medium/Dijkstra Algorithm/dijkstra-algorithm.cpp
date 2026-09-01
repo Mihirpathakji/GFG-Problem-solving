6cf5bf4c9949590a9739aece8438da5e5859c221
Mihir Pathakji <mihirgithub123456@gmail.com>
2026-09-01 16:12:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -1,82 +1,64 @@
 class Solution {
   public:
-  
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+
+        vector<int>visited(V,0);
         
-        unordered_map<int,vector<pair<int,int>>>adj_list;
-        
-        for(int i = 0;i < edges.size();i++) {
+        unordered_map<int,vector<pair<int,int>>>mp;
+        for(auto vec : edges) {
             
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int weight = edges[i][2];
+            int u = vec[0];//0
+            int v = vec[1];//1
             
-            adj_list[u].push_back({v,weight});
-            adj_list[v].push_back({u,weight});
+            int weight = vec[2];//1
+            
+            mp[u].push_back({v,weight});
+            mp[v].push_back({u,weight});
             
         }
         
-        vector<int>ans_distance(V,INT_MAX);//[]
         
-        vector<int>visited(V,0);//[]
+        vector<int>shortest_distance(V,INT_MAX);
+        shortest_distance[src] = 0;
         
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-       
-        int parent_node = -1;
-        pq.push({0,{src,parent_node}});//{0,{0,-1}}.
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>Min_heap;
+        //dist to src, node.
         
-        ans_distance[src] = 0;//
-
-        while(!pq.empty()) {
+        
+        Min_heap.push({0,src});
+        
+        while(!Min_heap.empty()) {
             
-            int weight = pq.top().first;//0.4.
-            int curr_node = pq.top().second.first;//0.1.
-            int parent_node = pq.top().second.second;//-1.0.
+            int node = Min_heap.top().second;
+            Min_heap.pop();
             
-            //In the priority_Queue.We are pushing the weight from the 
-            //parent node of that particular node.
-            //But if we wanted is the *Min_distance from the source node.
-        
-            pq.pop();
-            
-            if(visited[curr_node] == 1) {
+            if(visited[node] == 1) {
                 continue;
-            }            
-            
-            else {
-            
-                visited[curr_node] = 1;//1. 
-                
-                for(auto it : adj_list[curr_node]) {
-                    
-                    int u = it.first;//1. 4. 2.
-                    int weight = it.second;//4. 6. 6.
-                     
-                    //If the adjacent element is not visited than we need to 
-                    //push the minimum distance of that node from the source node.
-                    
-                    if(visited[u] == 0) {
-                        //i.e it's shortest distance from the source node is not yet finalized.
-                        
-                        if(ans_distance[u] > ans_distance[curr_node] + weight) {
-                            ans_distance[u] = ans_distance[curr_node] + weight;
-                        }
-                        
-                        pq.push({ans_distance[u],{u,curr_node}});
-                        
-                        //Now the ans_distance[u] will be the shortest distance
-                        //of that node u from the source node.
-                    }
-        
-                }
-            
             }
             
+            visited[node] = 1;
+            
+            //Relax the Unvisited adjacent node.
+            
+            for(auto vec : mp[node]) {
+                
+                int v = vec.first;
+                int weight = vec.second;
+                
+                if(visited[v] == 1) {
+                    continue;
+                }
+                
+                if(shortest_distance[v] > shortest_distance[node] + weight) {
+                    shortest_distance[v] = shortest_distance[node] + weight;
+                    Min_heap.push({shortest_distance[v],v});
+                }
+            }
+        
         }
         
-        
-        return ans_distance;
-        
-        
+        return shortest_distance;
+    
+
     }
 };
